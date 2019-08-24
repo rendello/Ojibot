27605a09910bd08dca9a05b7bca7ba7823748bf7
Gaven Rendell <gavenrendell@protonmail.com>
2019-08-24 12:45:06 -0400

    adds support for any word. adds functions

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3.6
 
 import io
+from urllib.parse import quote_plus
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-element = driver.get('https://ojibwe.lib.umn.edu/search?utf8=%E2%9C%93&q=anishinaabe&commit=Search&type=ojibwe')
-element = driver.find_element_by_class_name("main-entry-search").screenshot_as_png 
+def get_screenshot(ojibwe_word):
+    encoded_word = quote_plus(ojibwe_word)
 
-with open("img.png", "wb") as png:
-    png.write(element)
+    driver = webdriver.Firefox()
+    element = driver.get(f'https://ojibwe.lib.umn.edu/search?utf8=%E2%9C%93&q={encoded_word}&commit=Search&type=ojibwe')
+    png_data = driver.find_element_by_class_name("main-entry-search").screenshot_as_png 
+    #driver.close()
+    return png_data
 
-#from selenium.webdriver.common.keys import Keys
-#
-#driver = webdriver.Firefox()
-#driver.get("http://www.python.org")
-#assert "Python" in driver.title
-#elem = driver.find_element_by_name("q")
-#elem.clear()
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.RETURN)
-#assert "No results found." not in driver.page_source
-#driver.close()
+def write_png_to_file(png_data, filename):
+    with open("img.png", "wb") as png:
+        png.write(png_data)
+
+
+if __name__ == "__main__":
+    png_data = get_screenshot('mooz')
+    write_png_to_file(png_data, 'img.png')
+
