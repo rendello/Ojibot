65ca9c4bd0767237f38593709b27a4c69ea9a9ec
Gaven Rendell <gavenrendell@protonmail.com>
2019-08-24 18:48:10 -0400

    all functions work together, adds context managers a'plenty

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ from random import randint
 from selenium import webdriver
 import sqlite3
 
+from db_context_manager import dbopen
 
-conn = sqlite3.connect('words.db')
-c = conn.cursor()
 
 def fetch_random_entry(db_file):
-    c.execute("SELECT MAX(rowid) FROM words;")
-    rand_id = randint(0, c.fetchone()[0])
+    with dbopen('words.db') as c:
+        c.execute("SELECT MAX(rowid) FROM words;")
+        max_id = c.fetchone()[0]
+        rand_id = randint(0, max_id)
 
-    c.execute("SELECT title, url FROM words WHERE rowid=(?)", (rand_id, ))
-    results = c.fetchone()
-    title = results[0]
-    url = results[1]
-    return (title, url)
+        c.execute("SELECT title, url FROM words WHERE rowid=(?)", [rand_id])
+        results = c.fetchone()
+        title = results[0]
+        url = results[1]
+
+        return (title, url)
 
 
-def get_screenshot(ojibwe_word):
-    driver = webdriver.Firefox()
-    element = driver.get(f'https://ojibwe.lib.umn.edu/main-entry/{word_url}')
-    word_png_data = driver.find_element_by_class_name("main-entry-search").screenshot_as_png
-    #roots_png_data = driver.find_element_by_id("wordParts").screenshot_as_png
-    #driver.close()
-    return png_data
+def get_screenshot(word_url):
+    with webdriver.Firefox() as wd:
+        element = wd.get(f'https://ojibwe.lib.umn.edu/main-entry/{word_url}')
+        #word_png_data = wd.find_element_by_class_name("main-entry-search").screenshot_as_png
+        roots_png_data = wd.find_element_by_id("wordParts").screenshot_as_png
+        return roots_png_data
 
 
 def write_png_to_file(png_data, filename):
@@ -35,7 +36,7 @@ def write_png_to_file(png_data, filename):
 
 
 if __name__ == "__main__":
-    print(fetch_random_entry('words.db'))
-    #png_data = get_screenshot('adaawe')
-    #write_png_to_file(png_data, 'img.png')
+    title, url = fetch_random_entry('words.db')
+    png_data = get_screenshot(url)
+    write_png_to_file(png_data, 'img.png')
 
