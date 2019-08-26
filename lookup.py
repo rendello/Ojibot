7da34d436f80e68bd53821317f01a81f8277b6b2
Gaven Rendell <gavenrendell@protonmail.com>
2019-08-26 11:21:54 -0400

    continues with Discord formatting

--- lookup.py
+++ lookup.py
@@ -15,7 +15,7 @@ def fetch_oji_word_info(word_url):
         html as values, or None if not found.
     '''
     source = urllib.request.urlopen(f"https://ojibwe.lib.umn.edu/main-entry/{word_url}").read()
-    soup = bs.BeautifulSoup(source, 'html5lib')
+    soup = bs.BeautifulSoup(source, 'html.parser')
 
     sections = {}
 
@@ -27,24 +27,69 @@ def fetch_oji_word_info(word_url):
     return sections
 
 
-class DiscordFormat():
-    def lemma(s):
-        clean_text = BeautifulSoup(s, "lxml").text
-        formatted = f'# {clean_text}'
+def grab_links(soup):
+    ''' Grabs all link text and urls from soup.
+    
+    Args:
+        soup: a bs.BeautifulSoup() <object>.
+    
+    Returns:
+        links: <list> of <dict>s, each containing a 'text' and 'url' pair.
+    '''
+    links = []
+    for link in soup.find_all('a'):
+        links.append({'text':link.text, 'url':link.get('href')})
 
-        return formatted
+    return links
 
-    def gloss(s):
-        clean_text = BeautifulSoup(s, "lxml").text
-        formatted = f'{clean_text}'
+def lemma(s):
+    clean_text = bs.BeautifulSoup(str(s), 'html.parser').text
+    formatted = f'# {clean_text}'
 
-        return formatted
+    return formatted
+
+def gloss(s):
+    clean_text = bs.BeautifulSoup(str(s), 'html.parser').text
+    formatted = f'{clean_text}'
+
+    return formatted
+
+def word_parts(s):
+    soup = bs.BeautifulSoup(str(s), "html.parser")
+    
+    links = grab_links(soup)
+
+    # changes the unsplit word <strong>word</strong> to a markdown
+    # **word**. The html tags can be stripped since they'll all be stripped
+    # later anyway
+    original_word_html = soup.find('strong')
+    original_word = original_word_html.text
+    original_word_html.replace_with(f'**{original_word}**')
+
+    # cleans / stylizes links. Unrelated to grab_links()
+    [link.replace_with(f'*{link.text}*') for link in soup.find_all('a')]
+
+    formatted = soup.text
+
+    # for some reason, bs changes <em>it</em> to <em>h/</em>. This will not do.
+    formatted = formatted.replace('h/', '*it*')
+        
+    print(links)
+    return formatted
+
+
+
 
 
 
 s = fetch_oji_word_info('apiichishim-vta')
 
 for k, v in s.items():
-    print(f'{k}\n---\n{v}\n\n\n')
+    #print(f'{k}\n---\n{v}\n\n\n')
+    pass
 
-    
+#print(lemma(s['lemma']))
+#print('---')
+#print(gloss(s['gloss']))
+#print('---')
+print(word_parts(s['word_parts']))
