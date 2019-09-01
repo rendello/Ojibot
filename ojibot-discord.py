7d8b81352483e3a4b445d570f239c418b7e90bcf
Gaven Rendell <gavenrendell@protonmail.com>
2019-09-01 18:39:34 -0400

    !oji -> !

--- ojibot-discord.py
+++ ojibot-discord.py
@@ -9,16 +9,30 @@ from Core.lookup import get_word_urls, fetch_oji_word_info
 from Core.db_lookup import fuzzy_match
 from Core.normalize import to_rough_fiero
 
-bot = commands.Bot(command_prefix='!oji ')
+bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def oji(ctx, word):
+    ''' Gets a supposed Ojibwe word from the chat, and gives the definiton and
+    other word information. Tries to find the closest word if the spelling's
+    not exactly the same.
+
+    Args:
+        ctx: context, needed for Discord
+        word: <str>, a supposed Ojibwe word.
+
+    Returns:
+        <None>
+    '''
     urls = get_word_urls(word)
+
+    # Find the closest word if the specific requested spelling's not in db
     if urls == []:
         word = to_rough_fiero(word)
-        word = fuzzy_match(word, 1)[0]['word']
-        print(word)
-    urls = get_word_urls(word)
+        word = fuzzy_match(word, 1)[0]['word'] # Single element list containing dict
+        urls = get_word_urls(word)
+
+    # Sometimes there's multiple words with the same definition
     for url in urls:
         info = fetch_oji_word_info(url)
         string = fmt_dict_to_text(fmt_all(info))
